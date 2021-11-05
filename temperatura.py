b622c7c6983fcaf246520fe00dec1eab749eb200
SalasTec13 <89493294+SalasTec13@users.noreply.github.com>
2021-11-04 17:47:04 -0700

    Create temperatura.py

---
+++ temperatura.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+while True:
+# Toma lecturas de los tres sensores
+  
+  t = sense.get_temperature() # temperatura
+  p = sense.get_pressure() # presión
+  h = sense.get_humidity() # humedad
+  # Redondea los valores a un lugar decimal
+  t = round(t, 1)
+  p = round(p, 1)
+  h = round(h, 1)
+  # Crea el mensaje
+  # str() convierte el valor en una cadena de caracteres, para que pueda ser concatenado
+  message = "Temperatura: " + str(t) + " Presión: " + str(p) + " Humedad: " + str(h)
+  # Muestra el mensaje
+  sense.show_message(message, scroll_speed=0.05)
+  
